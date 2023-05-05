26e25023fa78a756d9e3b704a095baedeea68adc
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-05-05 19:36:22 +0200

    bump theme to beta 208

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -42,7 +42,7 @@ export default hopeTheme({
     //   mapping: 'title',
     // },
     mdEnhance: {
-      linkCheck: 'always',
+      checkLinks: 'always',
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/align.html
       align: true,
       //See https://vuepress-theme-hope.github.io/v2/guide/markdown/attrs.html
